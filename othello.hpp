b5daddd650f2a3f58120f4a83d63d6f5de186306
Daanya Anand <daanyaanand@gmail.com>
2018-04-20 03:47:22 -0700

    Create othello.hpp

---
+++ othello.hpp
@@ -0,0 +1,55 @@
+# ifndef DSAOTHELLO_HPP
+# define DSAOTHELLO_HPP
+
+# include "OthelloAI.hpp"
+# include <vector>
+
+using namespace std;
+
+namespace anandds
+{
+
+  class DSAOthello : public OthelloAI
+  {
+    
+  public:
+
+    /** Default constructor for DSAOthello AI alg
+     */
+    DSAOthello();
+    
+    
+    /** Returns vector of pairs holding coordinates for possible moves
+     */
+    vector<pair<int,int>> possible_moves(const OthelloGameState& state);
+
+
+    /** Evaluates current gamestate board 
+     */ 
+    int evaluate(const OthelloGameState& state);
+    
+    
+    /** Searches down tree to evaluate what the ideal move is
+     */
+    int recursive_search(OthelloGameState& state, int depth);
+
+
+    /** Keeps track of best move as algorithm checks all valid moves
+     */
+    //pair<int,int> best_move(const OthelloGameState& state,
+    //                        pair<int,int> current_move);
+ 
+    
+    /** Built on base class function in OthelloAI
+     */
+    virtual std::pair<int, int> chooseMove(const OthelloGameState& state);
+
+
+  private:
+    OthelloCell myturn;
+    
+  };
+ 
+}
+
+# endif // DSAOTHELLO_HPP
